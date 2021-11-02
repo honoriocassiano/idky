8df09953dab2b79abef0e9e03b399823c26f578c
Cassiano Honorio da Silva <honoriocassiano@gmail.com>
2021-11-02 16:44:59 -0300

    Add sdl window
    - Reduce scope of unsafe blocks
    - Add functions to print errors

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,50 @@
 use std::{thread, time};
-use std::ffi::CStr;
+use std::ffi::{CStr, CString};
+use std::os::raw::c_int;
+use std::thread::sleep;
 
 use sdl::*;
 
-fn main() {
+fn get_error() -> Option<String> {
     unsafe {
-        let status = SDL_Init(SDL_INIT_VIDEO);
-
-        if status < 0 {
-            let error = CStr::from_ptr(SDL_GetError()).to_str().unwrap();
-
-            println!("Error initializing SDL: {}", error);
-        } else {
-            println!("Ok");
-        }
+        CStr::from_ptr(SDL_GetError()).to_str().ok()
+            .map(|str| str.to_owned())
     }
+}
+
+fn print_if_error(message: &'static str) {
+    if let Some(err) = get_error() {
+        println!("{}: {}", message, err);
+    }
+}
+
+fn main() {
+    let status = unsafe { SDL_Init(SDL_INIT_VIDEO) };
+
+    if status < 0 {
+        print_if_error("Error initializing SDL");
+
+        return;
+    }
+
+    let window_name = CString::new("Hello World").unwrap();
+
+    let window = unsafe {
+        SDL_CreateWindow(window_name.as_ptr(),
+                         0 as c_int,
+                         0 as c_int,
+                         800,
+                         600,
+                         0) // No flags
+    };
+
+    if window.is_null() {
+        print_if_error("Error initializing window");
+
+        return;
+    }
+
+    sleep(time::Duration::from_secs(10));
+
+    unsafe { SDL_DestroyWindow(window) };
 }
