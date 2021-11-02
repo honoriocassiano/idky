7dffb6436954523692a6fe5e4d78d00f45e4e323
Cassiano Honorio da Silva <honoriocassiano@gmail.com>
2021-11-02 20:50:42 -0300

    Add Backend and Mode options to window initialization

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use std::{thread, time};
-use std::ffi::{CStr, CString};
-use std::os::raw::c_int;
-use std::thread::sleep;
 
 use window::Window;
+
+use crate::window::{Backend, Mode};
 
 mod window;
 
 fn main() {
-    let _window = Window::new("Hello", 800, 600);
+    let _window = Window::new("Hello", 800, 600, Backend::Metal, Mode::Window);
 
-    sleep(time::Duration::from_secs(10));
+    thread::sleep(time::Duration::from_secs(10));
 }
--- src/window/window.rs
+++ src/window/window.rs
@@ -1,7 +1,11 @@
 use std::ffi::{CStr, CString};
-use std::ops::Deref;
 
-use sdl::{SDL_CreateWindow, SDL_DestroyWindow, SDL_Init, SDL_INIT_EVERYTHING, SDL_INIT_VIDEO, SDL_Window};
+use sdl::{
+    SDL_CreateWindow, SDL_DestroyWindow, SDL_Init, SDL_INIT_EVERYTHING, SDL_INIT_VIDEO,
+    SDL_Window, SDL_WindowFlags,
+    SDL_WindowFlags_SDL_WINDOW_BORDERLESS, SDL_WindowFlags_SDL_WINDOW_FULLSCREEN,
+    SDL_WindowFlags_SDL_WINDOW_METAL, SDL_WindowFlags_SDL_WINDOW_OPENGL, SDL_WindowFlags_SDL_WINDOW_VULKAN,
+};
 
 pub struct Window {
     window: *mut SDL_Window,
@@ -9,14 +13,31 @@ pub struct Window {
 
 fn get_error() -> Option<String> {
     unsafe {
-        CStr::from_ptr(sdl::SDL_GetError()).to_str().ok()
+        CStr::from_ptr(sdl::SDL_GetError())
+            .to_str()
+            .ok()
             .map(|str| str.to_owned())
     }
 }
 
-impl Window {
-    pub fn new(title: &str, width: i32, height: i32) -> Window {
+#[allow(dead_code)]
+#[repr(u32)]
+pub enum Backend {
+    Metal = SDL_WindowFlags_SDL_WINDOW_METAL,
+    Opengl = SDL_WindowFlags_SDL_WINDOW_OPENGL,
+    Vulkan = SDL_WindowFlags_SDL_WINDOW_VULKAN,
+}
 
+#[allow(dead_code)]
+#[repr(u32)]
+pub enum Mode {
+    Window = 0,
+    Fullscreen = SDL_WindowFlags_SDL_WINDOW_FULLSCREEN,
+    Borderless = SDL_WindowFlags_SDL_WINDOW_BORDERLESS,
+}
+
+impl Window {
+    pub fn new(title: &str, width: i32, height: i32, backend: Backend, mode: Mode) -> Window {
         // TODO Split SDL initialization to another file
         let status = unsafe { SDL_Init(SDL_INIT_EVERYTHING) };
 
@@ -25,23 +46,15 @@ impl Window {
         }
 
         let window_name = CString::new(title).unwrap();
+        let flags = backend as u32 | mode as u32;
 
-        let window = unsafe {
-            SDL_CreateWindow(window_name.as_ptr(),
-                             0,
-                             0,
-                             width,
-                             height,
-                             0) // TODO Add flags
-        };
+        let window = unsafe { SDL_CreateWindow(window_name.as_ptr(), 0, 0, width, height, flags) };
 
         if window.is_null() {
             panic!("Error initializing window: {}", get_error().unwrap());
         }
 
-        Window {
-            window
-        }
+        Window { window }
     }
 }
 
