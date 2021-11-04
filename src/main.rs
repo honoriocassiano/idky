065acf79daf0db70e888f9668653287b988d0071
Cassiano Honorio da Silva <honoriocassiano@gmail.com>
2021-11-03 23:27:45 -0300

    Add handler example to main

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
+use std::sync::Arc;
 use std::{thread, time};
 
 use window::Window;
 
 use crate::core::System;
-use crate::window::{Backend, Mode};
+use crate::window::{Backend, Event, EventHandler, Mode, WindowControlFlow};
 
 mod core;
 mod window;
 
+struct HandlerExample {}
+
+impl EventHandler for HandlerExample {
+    fn handle(&mut self, _event: &Event) -> WindowControlFlow {
+        todo!()
+    }
+}
+
 fn main() {
     let mut system = System::new();
 
-    let _window = Window::new(&mut system, "Hello", 800, 600, Backend::Metal, Mode::Window);
+    let mut window = Window::new(&mut system, "Hello", 800, 600, Backend::Metal, Mode::Window);
+
+    window.add_handler(Arc::new(HandlerExample {}));
 
     thread::sleep(time::Duration::from_secs(10));
 }
