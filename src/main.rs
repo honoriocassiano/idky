f6de956a4cf43beb6e454427192e336d7963124d
Cassiano Honorio da Silva <honoriocassiano@gmail.com>
2021-11-24 09:04:07 -0300

    Set window backend to Vulkan

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,14 @@ mod window;
 fn main() {
     let mut system = System::new();
 
-    let mut window = Window::new(&mut system, "Hello", 800, 600, Backend::Metal, Mode::Window);
+    let mut window = Window::new(
+        &mut system,
+        "Hello",
+        800,
+        600,
+        Backend::Vulkan,
+        Mode::Window,
+    );
 
     loop {
         if let WindowControlFlow::Exit = window.handle_events() {
